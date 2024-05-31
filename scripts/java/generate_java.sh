3e8efdaa06bb9a8e4babdf96819e87f34e508de0
Andreas Ommundsen <anthomm@live.com>
2024-05-31 09:11:26 +0200

    feat: complete java protoc functionality;

--- scripts/java/generate_java.sh
+++ scripts/java/generate_java.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # This script is supposed to be copied into a Dockerfile, not run directly.
-mkdir java
+mkdir -p java/java
+mkdir -p java/grpc
 protoc ./proto/**/*.proto \
   --plugin=protoc-gen-grpc-java=/protoc-gen-grpc-java \
-  --grpc-java_out=./java
+  --java_out=./java/java \
+  --grpc-java_out=./java/grpc
